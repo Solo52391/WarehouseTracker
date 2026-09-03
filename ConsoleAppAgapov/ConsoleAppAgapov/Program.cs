a2dc2cd41133e2fd882d8c3f0792c51ecafbec81
Agapov Valentin <agapovv495@gmail.com>
2026-09-03 11:57:39 +0300

    Добавить вывод названия приложения

--- ConsoleAppAgapov/ConsoleAppAgapov/Program.cs
+++ ConsoleAppAgapov/ConsoleAppAgapov/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Проект: WarehouseTracker");
+Console.WriteLine("Студент: Агапов Валентин"); 
+Console.WriteLine("Складской учёт товаров");
