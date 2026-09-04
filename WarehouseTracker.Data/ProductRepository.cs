0b45a89b1250c48c470b053c10102047fc72fd8f
Agapov Valentin <agapovv495@gmail.com>
2026-09-04 11:19:13 +0300

    Добавить модель и хранилище данных

---
+++ WarehouseTracker.Data/ProductRepository.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+public class ProductRepository
+{
+    private readonly List<Product> _items = new()
+    {
+        new Product { Id = 1, Name = "Ноутбук", Quantity = 5 },
+        new Product { Id = 2, Name = "Мышь", Quantity = 15 },
+        new Product { Id = 3, Name = "Клавиатура", Quantity = 8 },
+        new Product { Id = 4, Name = "Монитор", Quantity = 3 }
+    };
+
+    public List<Product> GetAll()
+    {
+        return _items;
+    }
+}
