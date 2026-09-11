c92437cfe1cbc7fc8cd2327c0ae258a96aaeab39
Agapov Valentin <agapovv495@gmail.com>
2026-09-11 11:15:54 +0300

    Добавить выбор хранилища параметром запуска

--- ConsoleAppAgapov/ConsoleAppAgapov/Program.cs
+++ ConsoleAppAgapov/ConsoleAppAgapov/Program.cs
@@ -1,14 +1,32 @@
-﻿using System;
+﻿
+using System;
+using System.IO;
 using WarehouseTracker.Data;
 using WarehouseTracker.Logic;
 
-IProductRepository repository = new ProductRepository();
+string jsonPath = Path.Combine(AppContext.BaseDirectory, "products.json");
+string xmlPath = Path.Combine(AppContext.BaseDirectory, "products.xml");
 
+string kind = args.Length > 0 ? args[0] : "json";
+
+IProductRepository repository = kind switch
+{
+    "xml" => new XmlProductRepository(xmlPath),
+    "memory" => new ProductRepository(),
+    _ => new JsonProductRepository(jsonPath)
+};
+
+Console.WriteLine($"Хранилище: {kind}");
 var service = new ProductService(repository);
+
+Console.WriteLine("Название нового товара: ");
+string name = Console.ReadLine() ?? "";
+service.AddProduct(name, 5);
 
 Console.WriteLine("Товары с количеством меньше 10:");
 foreach (var item in service.GetLowStock())
 {
-    Console.WriteLine($"{item.Id}: {item.Name} - {item.Quantity} шт.");  
+    Console.WriteLine($"{item.Id}: {item.Name} - {item.Quantity} шт.");
 }
 
+Console.WriteLine($"Суммарное количество товаров: {service.GetTotalQuantity()}");
